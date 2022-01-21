009e6b9e9b8038e86f0b518353c2cf0517f6e96a
mtisby <marieltisby1998@gmail.com>
2022-01-21 13:19:34 -0800

    deploy

--- cinema-save-client/src/index.js
+++ cinema-save-client/src/index.js
@@ -23,12 +23,12 @@ render(
     <HashRouter>
         <Routes>
           <Route exact path="/" element={<App />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/profile/:id" element={<Profile />} />
-          <Route path="/profile/:id/board/:id" element={<ShowIndBoard />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/movie/:id" element={<ShowMovie />} />
+          <Route path="/cinema-save-client/home" element={<Home />} />
+          <Route path="/cinema-save-client/profile/:id" element={<Profile />} />
+          <Route path="/cinema-save-client/profile/:id/board/:id" element={<ShowIndBoard />} />
+          <Route path="/cinema-save-client/register" element={<Register />} />
+          <Route path="/cinema-save-client/login" element={<Login />} />
+          <Route path="/cinema-save-client/movie/:id" element={<ShowMovie />} />
         </Routes>
     </HashRouter>,
   rootElement   
