44f0aa8a1a41bcc12ae1b0bfd2af84818d89c599
mtisby <marieltisby1998@gmail.com>
2022-01-21 12:27:47 -0800

    deploy

--- cinema-save-client/src/index.js
+++ cinema-save-client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from "react-dom";
 import {
-  HasherRouter,
+  BrowserRouter,
   Routes,
   Route
 } from "react-router-dom";
@@ -20,7 +20,7 @@ import ShowIndBoard from './pages/ShowIndBoard'
 
 const rootElement = document.getElementById("root");
 render(
-    <HasherRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path="/" element={<App />} />
           <Route path="/home" element={<Home />} />
@@ -30,6 +30,6 @@ render(
           <Route path="/login" element={<Login />} />
           <Route path="/movie/:id" element={<ShowMovie />} />
         </Routes>
-    </HasherRouter>,
+    </BrowserRouter>,
   rootElement   
 );
