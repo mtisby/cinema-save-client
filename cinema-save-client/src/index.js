9b5b9b6f3f24e98d8e02334be0b954b2cf2dc534
mtisby <marieltisby1998@gmail.com>
2022-01-21 12:28:46 -0800

    deploy

--- cinema-save-client/src/index.js
+++ cinema-save-client/src/index.js
@@ -20,7 +20,7 @@ import ShowIndBoard from './pages/ShowIndBoard'
 
 const rootElement = document.getElementById("root");
 render(
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
         <Routes>
           <Route path="/" element={<App />} />
           <Route path="/home" element={<Home />} />
