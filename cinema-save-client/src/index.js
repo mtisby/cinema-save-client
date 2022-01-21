51333d3afefe5c9f2a11e13c54e5dfe94e2024b8
mtisby <marieltisby1998@gmail.com>
2022-01-21 12:35:06 -0800

    deploy

--- cinema-save-client/src/index.js
+++ cinema-save-client/src/index.js
@@ -22,13 +22,13 @@ const rootElement = document.getElementById("root");
 render(
     <HashRouter>
         <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/profile/:id" element={<Profile />} />
-          <Route path="/profile/:id/board/:id" element={<ShowIndBoard />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/movie/:id" element={<ShowMovie />} />
+          <Route path={process.env.PUBLIC_URL + "/"} element={<App />} />
+          <Route path={process.env.PUBLIC_URL + "/home"} element={<Home />} />
+          <Route path={process.env.PUBLIC_URL + "/profile/:id"} element={<Profile />} />
+          <Route path={process.env.PUBLIC_URL + "/profile/:id/board/:id"} element={<ShowIndBoard />} />
+          <Route path={process.env.PUBLIC_URL + "/register"} element={<Register />} />
+          <Route path={process.env.PUBLIC_URL + "/login"} element={<Login />} />
+          <Route path={process.env.PUBLIC_URL + "/movie/:id"} element={<ShowMovie />} />
         </Routes>
     </HashRouter>,
   rootElement   
