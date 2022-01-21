6548837e24eae8d7e6394e557a2815b3a10264e4
mtisby <marieltisby1998@gmail.com>
2022-01-21 13:38:54 -0800

    deploy

--- cinema-save-client/src/App.js
+++ cinema-save-client/src/App.js
@@ -1,12 +1,18 @@
+import { Link } from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
       <h1>Welcome to Splash Page</h1>
 
       <br/>
-      <a href="/register">register</a>
+      <Link className='navbar-link' to="/register">
+          <p>Register</p>
+      </Link>
       <br/>
-      <a href="/cinema-save-client/login">login</a>
+      <Link className='navbar-link' to="/login">
+          <p>Login</p>
+      </Link>
       <br />
     </div>
   );
