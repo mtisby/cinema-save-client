fb4abbc0a8ad0df56b21afa19fac2e0724241493
mtisby <marieltisby1998@gmail.com>
2022-02-28 14:28:13 -0800

    update login

--- cinema-save-client/src/App.js
+++ cinema-save-client/src/App.js
@@ -1,19 +1,30 @@
 import { Link } from 'react-router-dom';
+import "./App.css"
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to Splash Page</h1>
+    <div className="App flex-col-center">
+      <div className='shadow'></div>
+      <div className='app-bgimg flex-col-top'></div>
 
-      <br/>
-      <Link to="/register">
-          <p>Register</p>
-      </Link>
-      <br/>
-      <Link to="/login">
-          <p>Login</p>
-      </Link>
-      <br />
+      <div className='content'>
+        <h1>Welcome to Cinema Save</h1>
+        
+        <div className='flex-row-center'>
+          <div className='splash-link'>
+            <Link to="/register">
+                <p>Register</p>
+            </Link>
+          </div>
+          
+          <div className='splash-link'>
+            <Link to="/login">
+                <p>Login</p>
+            </Link>
+          </div>
+        </div>
+        
+      </div>
     </div>
   );
 }
